2ce1f728d106f669361f51c62385d6b781ee890d
José Carlos Quiroga Garre <josecquiroga@gmail.com>
2016-02-04 20:43:22 +0100

    no message

--- EjemploCustomControls/EjemploCustomControls.iOS/CustomControls/IosLabel.cs
+++ EjemploCustomControls/EjemploCustomControls.iOS/CustomControls/IosLabel.cs
@@ -7,13 +7,13 @@ using Xamarin.Forms.Platform.iOS;
 [assembly: ExportRenderer(typeof(CustomLabel), typeof(IosLabel))]
 namespace EjemploCustomControls.iOS.CustomControls
 {
-    public class IosLabel:LabelRenderer
+    public class IosLabel : LabelRenderer
     {
         protected override void OnElementChanged(
          ElementChangedEventArgs<Label> e)
         {
             base.OnElementChanged(e);
-            Control.TextColor= UIColor.Blue;
+            Control.TextColor = UIColor.Blue;
         }
     }
 }
